3eb455af6e8735868d15d84fc368fb1af2e7067a
swihart <bruce.swihart@gmail.com>
2014-11-05 07:20:28 -0500

    simplify lasagna.r

--- R/lasagna.R
+++ R/lasagna.R
@@ -4,8 +4,7 @@
 #' @param col colors
 #' @param axes (logical) axes on or off?
 #' @param ... Additional stuff to be passed to \code{image}
-lasagna<- function(X, col=rev(sequential_hcl(100, h=120, c = 80, power = 2.2)[1:100]), 
-                   axes=FALSE, ...){
+lasagna<- function(X, col=rainbow_hcl(100), axes=FALSE, ...){
   image(t(X)[,(nrow(X):1)], col=col, axes=axes, ...  )
   box()
 }
