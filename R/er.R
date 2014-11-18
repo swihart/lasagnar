2a255acf697e3f304e0cc2d2f0c71c94dde016c0
swihart <bruce.swihart@gmail.com>
2014-11-18 06:47:03 -0500

    orderVar scalar

--- R/er.R
+++ R/er.R
@@ -1,12 +1,12 @@
 #' @name er
 #' @title entire-row sorting 
 #' @param X a matrix: each row a subject, each column a time/location across subjects
-#' @param orderVar a vector with the unique elements of X presented in the order of the desired
-#' resultant sorting
+#' @param orderVar a scalar; one of the values in X upon which the percentage of occurence will determine the ordering of the rows. 
+#' Defaults to the minimum value of X
 #'  @param naLast defaults to false; should NA values be sorted to first position or last. Defaults to FALSE.
 #' 
 ## entire row sort
-er <- function(X,orderVar=sort(unique(X)), naLast=F){
+er <- function(X,orderVar=min(X,na.rm=TRUE), naLast=F){
   if (length(orderVar) == 1){
     perc <- apply( X, 1, function(W){ sum(W==orderVar)  })
     return(X[order(perc), ])
