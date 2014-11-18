ea1ae7ee261704c5d1e104b64b5c4b693662ef3a
swihart <bruce.swihart@gmail.com>
2014-11-18 06:36:25 -0500

    er add

---
+++ R/er.R
@@ -0,0 +1,15 @@
+#' @name er
+#' @title entire-row sorting 
+#' @param X a matrix: each row a subject, each column a time/location across subjects
+#' @param orderVar a vector with the unique elements of X presented in the order of the desired
+#' resultant sorting
+#'  @param naLast defaults to false; should NA values be sorted to first position or last. Defaults to FALSE.
+#' 
+## entire row sort
+er <- function(X,orderVar=c(), naLast=F){
+  if (length(orderVar) == 1){
+    perc <- apply( X, 1, function(W){ sum(W==orderVar)  })
+    return(X[order(perc), ])
+  }
+  X[order(orderVar), ]
+}
