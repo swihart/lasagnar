6f05bb59b8614c6dab6e07469fb6ff81aee3b97c
swihart <bruce.swihart@gmail.com>
2014-11-18 06:38:13 -0500

    orderVar default

--- R/er.R
+++ R/er.R
@@ -6,7 +6,7 @@
 #'  @param naLast defaults to false; should NA values be sorted to first position or last. Defaults to FALSE.
 #' 
 ## entire row sort
-er <- function(X,orderVar=c(), naLast=F){
+er <- function(X,orderVar=sort(unique(X)), naLast=F){
   if (length(orderVar) == 1){
     perc <- apply( X, 1, function(W){ sum(W==orderVar)  })
     return(X[order(perc), ])
