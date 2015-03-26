ad994249c1b939c307dd8862beb03f87c1bdcbc9
swihart <bruce.swihart@gmail.com>
2015-03-26 12:01:58 -0400

    add dfviewr

--- R/dfviewr.R
+++ R/dfviewr.R
@@ -1,12 +1,9 @@
 #' @name dfviewr
 #' @title data frame viewer
 #' @param df a dataframe 
-#' @param axes (logical) TRUE invokes the default axes from image().  FALSE invokes best guess (to be better defined later)
-#' @param main as in plot(), the text of the title.  Defaults to "(A)  Initial Lasagna Plot"
-#' @param main.adj the value for `adj` in title().  Defaults to 0, which is left-aligned.
-#' @param cex.axis the cex.axis value if `axes` is FALSE.
+#' @param col a vector of 5 colors.  The first is for dates, second for factors, third for characters, fourth for numeric, and fifth for NA.
 #' @param gridlines (logical) default TRUE.
-#' @param legend (logical) defaults to FALSE.  TRUE invokes image.plot() from fields package instead of image() and thus displays a legend.
+#' @param legend (logical) defaults to TRUE.
 #' @param ... Additional stuff to be passed to \code{image}
 dfviewr <- function(df, col=c("red","blue","green","white","black"), axes=FALSE, 
                        main="", main.adj=0, 
@@ -38,7 +35,7 @@ dfviewr <- function(df, col=c("red","blue","green","white","black"), axes=FALSE,
   row.names(mat.out) <- 1:nrow(df)
   
   
-  lasagna(mat.out, col=col, cex=0.67, main=main)
+  lasagna(mat.out, col=col, cex=0.67, main=main, gridlines=gridlines)
   if(legend){legend("bottom", fill=col,
                     legend=c("dates", "factors", "characters", "numeric", "NA"), 
                     horiz=T, xpd=NA, inset=c(-.15), border="black")}
