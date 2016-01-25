983697441ecb0690fbf3d9793d93ebab2ee40744
muschellij2 <muschellij2@gmail.com>
2016-01-24 23:53:30 -0800

    fixing rainbow

--- R/lasagna_plain.R
+++ R/lasagna_plain.R
@@ -4,8 +4,9 @@
 #' @param col colors.  See "Escaping RGB Land" and the vignette for the colorspace package
 #' for followable advice on colors.
 #' @param axes (logical) axes on or off?
+#' @import colorspace
 #' @param ... Additional stuff to be passed to \code{image}
-lasagna_plain<- function(X, col=rainbow_hcl(100), axes=FALSE, ...){
-  image(t(X)[,(nrow(X):1)], col=col, axes=axes, ...  )
+lasagna_plain <- function(X, col = rainbow_hcl(100), axes = FALSE, ...){
+  image(t(X)[,(nrow(X):1)], col = col, axes = axes, ...  )
   box()
 }
