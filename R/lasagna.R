4bc64bd8ea1542c9215d655e6a15d0e6a8339b42
muschellij2 <muschellij2@gmail.com>
2016-01-25 11:57:14 -0800

    Added arguments to lasagna.

--- R/lasagna.R
+++ R/lasagna.R
@@ -4,47 +4,63 @@
 #' @param X a matrix: each row a subject, each column a time/location across subjects; preferably rownames are the ids.
 #' @param col colors.  See "Escaping RGB Land" and the vignette for the colorspace package
 #' for followable advice on colors.
-#' @param axes (logical) TRUE invokes the default axes from image().  FALSE invokes best guess (to be better defined later)
+#' @param axes (logical) TRUE invokes the default axes from image().  
+#' FALSE invokes best guess, and better control using \code{xaxis/yaxis}.
+#' @param xaxis Use best guess x-axis if \code{image.axis = FALSE}
+#' @param yaxis Use best guess y-axis if \code{image.axis = FALSE}
 #' @param main as in plot(), the text of the title.  Defaults to "(A)  Initial Lasagna Plot"
 #' @param main.adj the value for `adj` in title().  Defaults to 0, which is left-aligned.
 #' @param cex.axis the cex.axis value if `axes` is FALSE.
-#' @param gridlines (logical) default TRUE.
+#' @param gridlines (logical) Add gridlines to the plot.  Useful if image is not
+#' too big.
 #' @param legend (logical) defaults to FALSE.  TRUE invokes image.plot() from fields package instead of image() and thus displays a legend.
 #' @param ... Additional stuff to be passed to \code{image}
 #' @importFrom fields image.plot
 #' @export 
-lasagna<- function(X, col=rainbow_hcl(length(unique(c(X)))), axes=FALSE, 
+lasagna <- function(X, col=rainbow_hcl(length(unique(c(X)))), 
+                   image.axes = FALSE, 
+                   xaxis = TRUE,
+                   yaxis = TRUE,
                    main="(A)  Initial Lasagna Plot", main.adj=0, 
                    cex.axis=1.75, 
-                   gridlines=TRUE,
+                   gridlines=FALSE,
                    legend=FALSE, ...){
-  if(!legend){
-  ## THE crucial piece:  everything else is just window dressing
-  image(t(X)[,(nrow(X):1)], col=col, axes=axes, useRaster = TRUE, ...  )
-  }else{
-  image.plot(t(X)[,(nrow(X):1)], col=col, axes=axes, ...  )
+  if (!legend) {
+    ## THE crucial piece:  everything else is just window dressing
+    image(t(X)[,(nrow(X):1)], col=col, axes=image.axes, useRaster = TRUE, ...  )
+  } else{
+    image.plot(t(X)[,(nrow(X):1)], col=col, axes=image.axes, useRaster = TRUE, ...  )
   }
   ## box border
   box()
   title(main, adj=main.adj)
-  if(!axes){
-  axis(1, seq(0,1,1/(ncol(X)-1)), 1:ncol(X), cex.axis=cex.axis, tck=0, mgp=c(0,.50,0))
-  axis(2, seq(0,1,1/(nrow(X)-1)), rev(rownames(X)),las=1,
-       cex.axis=cex.axis, tck=0, mgp=c(0,.2,0))
+  if(!image.axes){
+    if (xaxis){
+      axis(1, seq(0,1,1/(ncol(X)-1)), 
+           1:ncol(X), cex.axis=cex.axis, 
+           tck=0, mgp=c(0,.50,0))
+    }
+    if (yaxis){
+      axis(2, seq(0,1,1/(nrow(X)-1)), 
+           rev(rownames(X)), las=1,
+           cex.axis=cex.axis, tck=0, mgp=c(0,.2,0))
+    }
   }
   if(gridlines){
-  ## next two axis() calls add grid-lines:
-  axis(1,
-       seq( 1/(ncol(X)-1)*0.5, 1 - 1/(ncol(X)-1)*0.5, length=(ncol(X)-1)),##c(1/10,3/10,5/10,7/10,9/10),
-       labels=NA,
-       tck=1,
-       lty=1,
-       col="black") 
-  axis(2,
-       seq( 1/(nrow(X)-1)*0.5, 1 - 1/(nrow(X)-1)*0.5, length=(nrow(X)-1)),##c(1/6,3/6,5/6),
-       labels = NA,
-       tck=1,
-       lty=1,
-       col="black") 
+    ## next two axis() calls add grid-lines:
+    axis(1,
+         seq( 1/(ncol(X)-1)*0.5, 1 - 1/(ncol(X)-1)*0.5, 
+              length=(ncol(X)-1)),##c(1/10,3/10,5/10,7/10,9/10),
+         labels=NA,
+         tck=1,
+         lty=1,
+         col="black") 
+    axis(2,
+         seq( 1/(nrow(X)-1)*0.5, 1 - 1/(nrow(X)-1)*0.5, 
+              length=(nrow(X)-1)),##c(1/6,3/6,5/6),
+         labels = NA,
+         tck=1,
+         lty=1,
+         col="black") 
   }
 }
